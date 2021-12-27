c7b904a01acc04d65faf4a222421c40b5d2474da
zeroxbt <89495162+zeroxbt@users.noreply.github.com>
2021-12-27 18:20:15 +0100

    Delete lol.sql

--- models/lol.sql
+++
@@ -1,9 +0,0 @@
-"SELECT " + "\n" +
-"    i.Identity, " + "\n" +
-"    bc.Name BlockchainName, " + "\n" +
-"    i.Stake, " + "\n" +
-"    i.StakeReserved " + "\n" +
-"FROM Identity i" + "\n" +
-"    JOIN blockchain bc ON bc.id = i.blockchainid" + "\n" +
-"    JOIN OfferHolders oh ON oh.Holder = i.Identity" + "\n" +
-"GROUP BY oh.OfferId";
